dc094899e6d05abb71c74d70ae7bed18f0426f24
Annie Cook <anniecook@ostby.com>
2015-07-01 16:07:25 -0400

    finished routes for dropdowns

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var Promise = require('bluebird');
 
+
+var promisifiedFind = function (collection){
+  return new Promise (function (resolve, reject){
+    collection.find(function (err, data) {
+      if (err) reject(err);
+      else resolve(data);
+    })
+  })
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  
+  var promises = [
+    promisifiedFind(models.Hotel),
+    promisifiedFind(models.Restaurant),
+    promisifiedFind(models.ThingToDo)
+  ];
+  Promise.all(promises).spread( function (hotelData, restaurantData, todoData){
+    res.render('index', {hotels: hotelData, restaurants: restaurantData, todos: todoData});  
+  })
+
+
 });
 
 module.exports = router;
